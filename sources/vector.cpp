90108e0ddab35d0738ec12245353a530c6397446
MaximZhuravlyov <37067295+MaximZhuravlyov@users.noreply.github.com>
2018-03-28 17:21:11 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -3,58 +3,96 @@
 
 #include "vector.hpp"
 
-vector_t::vector_t() //afsaasf
+vector_t::vector_t()
 {
-	
+	size_ = 0;
+	capacity_ = 0;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
-}
+        for(size_t i=0;i<size_;++i)
+                elements_[i]=other.elements_[i];
+}//dim=size
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
+	if (this != &other)
+        {
+                delete[] elements_;
+                for (size_t i=0; i <= size_; ++i)
+                        elements_[i]=other.elements_[i];
+        }
 	return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	return false;
+	if(size_ != other.size_)
+		return false;
+	else
+		return true;
+	//return false;
 }
 
 vector_t::~vector_t()
 {
+	delete[] elements_;
 }
 
 std::size_t vector_t::size() const
 {
-    return 0;
+	return size_;
+    //return 0;
 }
 
 std::size_t vector_t::capacity() const
 {
-    return 0;
+	return capacity_;
+    //return 0;
 }
 
 void vector_t::push_back(int value)
 {
+	if(size_ > capacity_)
+		capacity_ *= 2;
+	size_++;
+	elements_[size-1]=value;
+	return;
 }
 
 void vector_t::pop_back()
 {
+	if(size_ != 0)
+	size_ -= 1;
+	else
+		return;
+	if((4 * size_) == capacity_)
+		capacity_ /= 2;
+	
 }
 
 int & vector_t::operator [](std::size_t index)
 {
-	return elements_[0];
+	if (index <= size_)
+                return elements_[index];
+        else
+               return 1;
+	//return elements_[0];
 }
 
 int vector_t::operator [](std::size_t index) const
 {
-	return 0;
+	if (index <= size_)
+                return 1;
+	return 1;
 }
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs)
 {
+	if(size_ != other.size_)
+		return true;
+	else
+		return false;
 	return true;
 }
